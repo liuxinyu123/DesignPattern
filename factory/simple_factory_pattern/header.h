cb0b1835ae002baa680027bb21838df2a005dfdd
liuyakun <happy_lucky52@163.com>
2017-03-21 21:38:44 +0800

    simple_factory_pattern

---
+++ factory/simple_factory_pattern/header.h
@@ -0,0 +1,94 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+typedef enum ProductTypeTag
+{
+	TypeA,
+	TypeB,
+	TypeC
+}ProductType;
+
+
+class Product//抽象类
+{
+
+public:
+		virtual void show () = 0;
+		virtual ~Product ()  {}
+};
+
+class ProductA : public Product
+{
+
+public:
+	   void show ()
+		{
+			cout << "I am a ProductA" << endl;
+		}	   
+	   ~ProductA () {}
+};
+
+class ProductB : public Product
+{
+
+public:
+	   void show ()
+		{
+			cout << "I am a ProductB" << endl;
+		}	   
+	   ~ProductB () {}
+};
+
+class ProductC : public Product
+{
+
+public:
+	   void show ()
+		{
+			cout << "I am a ProductC" << endl;
+		}	   
+	   ~ProductC () {}
+};
+
+class Factory 
+{
+
+public: 
+		Product* createProduct (ProductType type)//可以使用智能指针
+		{
+			switch (type)
+			{
+				case TypeA:
+							return new ProductA ();
+				case TypeB:
+							return new ProductB ();
+				case TypeC:
+							return new ProductC ();
+				default:
+							return nullptr;
+			}
+		}
+};
+
+class Factory_A
+{
+
+public: 
+		std::shared_ptr<Product> createProduct (ProductType type)//可以使用智能指针
+		{
+			switch (type)
+			{
+				case TypeA:
+							return std::make_shared<ProductA> (ProductA ());
+				case TypeB:
+							return std::make_shared<ProductB> (ProductB ());
+				case TypeC:
+							return std::make_shared<ProductC> (ProductC ());
+				default:
+							return nullptr;
+			}
+		}
+};
+
---
+++ factory/simple_factory_pattern/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "header.h"
+
+using std::cout;
+using std::endl;
+
+int main (int argc, char *argv[])
+{
+	Factory productFactory = Factory ();
+	Product* productObjA = productFactory.createProduct (TypeA);
+	Product* productObjB = productFactory.createProduct (TypeB);
+	Product* productObjC = productFactory.createProduct (TypeC);
+
+	if (productObjA)
+		productObjA -> show ();
+
+	if (productObjB)
+		productObjB -> show ();
+
+	if (productObjC)	
+		productObjC -> show ();
+
+	delete productObjA;
+	delete productObjB;
+	delete productObjC;
+
+	cout << "++++++++++++++++++++++++++++++++++++++" << endl;
+
+	Factory_A productFactory1 = Factory_A ();
+	auto productObjA1 = productFactory1.createProduct (TypeA);
+	auto productObjB1 = productFactory1.createProduct (TypeB);
+	auto productObjC1 = productFactory1.createProduct (TypeC);
+
+	if (productObjA1)
+		productObjA1 -> show ();
+
+	if (productObjB1)
+		productObjB1 -> show ();
+
+	if (productObjC1)	
+		productObjC1 -> show ();
+
+	return 0;
+}
